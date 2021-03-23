7d5ba2a2c78e2683d460da08d624c9ec36c8c298
Fredrika Lindh <fredrikalindh@gmail.com>
2021-03-23 13:40:39 -0600

    fb

--- Firebase/config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyD-kbYj3HCrVNxEuci-HNOxI0egDBjG1Co",
-  authDomain: "chat-app-4d149.firebaseapp.com",
-  projectId: "chat-app-4d149",
-  storageBucket: "chat-app-4d149.appspot.com",
-  messagingSenderId: "514142097468",
-  appId: "1:514142097468:web:6f10c8b391e8d622fb7339",
-  measurementId: "G-2TCHS6YBNT",
-};
